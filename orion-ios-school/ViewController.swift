66f320254529d9806b70812017dca55f895633c0
alexclay1001 <alexclay1001@gmail.com>
2021-06-24 16:28:17 +0300

    Initial Commit

---
+++ orion-ios-school/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  orion-ios-school
+//
+//  Created by Aleksandra Glinina on 24.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
